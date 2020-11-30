44f6d6125c69b9045c8729a9a4bd21ee91a82138
Mateusz Snochowski <snochowskimateusz@gmail.com>
2020-11-30 17:59:15 +0100

    Add list and question template

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from data_example import questions
 import data_processing
 
@@ -12,6 +12,20 @@ def index():
     question_keys = data_processing.get_keys(questions[0])
     return render_template('index.html', questions=questions, headers=headers, question_keys=question_keys)
 
+@app.route('/list')
+def list_of_question():
+    headers = ['User ID', 'Time of a submission', 'number of views', 'number of votes', 'type of a question',
+               'question message', 'question image']
+    question_keys = data_processing.get_keys(questions[0])
+    return render_template('list.html',questions=questions,headers=headers,question_keys=question_keys)
+
+@app.route("/question")
+def question():
+    headers = ['User ID','type of a question','question message', 'question image']
+    question_keys = ["id","type","message","image"]
+
+
+    return render_template('question.html', questions=questions,headers=headers,question_keys=question_keys)
 
 if __name__ == '__main__':
     app.run()
