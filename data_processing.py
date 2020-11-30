e657e28fb74609e23dda6b8bc725471a952e2375
tmedryk <tomekmedryk@gmail.com>
2020-11-30 18:12:03 +0100

    sort_data - sort dictionary by argument

--- data_processing.py
+++ data_processing.py
@@ -25,6 +25,14 @@ def get_dict_from_list(keys, values):
     return result
 
 
+# data - dictionary data, key = value from header as string, order - string 'ascending' or 'descending'
+def sort_data(data, key, order):
+    if order == "ascending":
+        return sorted(data, key=lambda x: x[key], reverse=False)
+    elif order == "descending":
+        return sorted(data, key=lambda x: x[key], reverse=True)
+
+
 def verify_lists_for_dict(keys_list, key_arguments):
     incorrect_tmp = [key_arguments[i] for i in range(len(key_arguments)) if len(keys_list) != len(key_arguments[i])]
 
